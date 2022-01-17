1cf70f2f04392ef162ecc628202a3cd175c5c05d
nrysbaev <nurkenrysbaev@gmail.com>
2022-01-18 04:51:57 +0600

    added POST, PUT, DELETE methods

--- shop/views.py
+++ shop/views.py
@@ -5,21 +5,51 @@ from . import serializers
 from . import models
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list_view(request):
-    products = models.Product.objects.all()
-    data = serializers.ProductListSerializer(products, many=True).data
-    return Response(data=data)
+    if request.method == 'GET':
+        products = models.Product.objects.all()
+        data = serializers.ProductListSerializer(products, many=True).data
+        return Response(data=data)
+    elif request.method == 'POST':
+        title = request.data['title']
+        description = request.data.get('description', '')
+        price = request.data['price']
+        category = request.data['category']
+        tags = request.data['tags']
+        product = models.Product.objects.create(
+            title=title, description=description, price=price, category_id=category
+        )
+        product.tags.set(tags)
+        return Response(data=serializers.ProductDetailSerializer(product).data,
+                        status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def products_detail_view(request, id):
     try:
         product = models.Product.objects.get(id=id)
     except models.Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    data = serializers.ProductDetailSerializer(product).data
-    return Response(data=data)
+        return Response(data={'message': 'NOT FOUND'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        data = serializers.ProductDetailSerializer(product).data
+        return Response(data=data)
+
+    elif request.method == 'PUT':
+        product.title = request.data.get('title', f'{product.title}')
+        product.description = request.data.get('description', f'{product.description}')
+        product.price = request.data.get('price', f'{product.price}')
+        product.category_id = request.data.get('category', f'{product.category_id}')
+        tags = request.data.get('tags', [tag for tag in product.tags.all()])
+        product.tags.set(tags)
+        product.save()
+        return Response(data=serializers.ProductDetailSerializer(product).data,
+                        status=status.HTTP_201_CREATED)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(data={'message': 'Product removed successfully!'})
 
 
 @api_view(['GET'])
