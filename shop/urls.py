1e48f4b6c9c9684df819627b2d634533f25cf91b
nrysbaev <nurkenrysbaev@gmail.com>
2022-01-25 05:34:36 +0600

    added user registration, authorization and activation via email

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from shop import views
+
+urlpatterns = [
+    path('api/v1/products/', views.products_list_view),
+    path('api/v1/products/<int:id>', views.products_detail_view),
+    path('api/v1/products/reviews/', views.products_reviews_view),
+    path('api/v1/products/tags/', views.products_tags_view),
+]
--- shop_backend/urls.py
+++ shop_backend/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from shop import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/products/', views.products_list_view),
-    path('api/v1/products/<int:id>', views.products_detail_view),
-    path('api/v1/products/reviews/', views.products_reviews_view),
-    path('api/v1/products/tags/', views.products_tags_view),
+    path('', include('shop.urls')),
+    path('', include('users.urls')),
 ]
