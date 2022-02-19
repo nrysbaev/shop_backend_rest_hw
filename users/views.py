500cbb2d50bd7b90e5cacc15e2111b04dbfd03cb
nrysbaev <nurkenrysbaev@gmail.com>
2022-02-19 17:18:29 +0600

    deleted test classes

--- users/views.py
+++ users/views.py
@@ -54,13 +54,6 @@ class ActivateAPIView(GenericAPIView):
         return Response(data={'User activated successfully!'})
 
 
-class SomeNewClass:
-    print('New class for branch test')
-
-
-class Test2:
-    pass
-
 # @api_view(['POST'])
 # def register(request):
 #     if request.method == 'POST':
